585a09a71ec9e397b67e8f68c950736e5533ccb0
Mariia Shulezhko <119418285+mshulezhko@users.noreply.github.com>
2024-01-27 08:31:21 +0100

    Update OperationButton.js
    delete space

--- src/OperationButton.js
+++ src/OperationButton.js
@@ -1,7 +1,5 @@
-
 import { ACTIONS } from "./App";
 
 export default function OperationButton({ operation, dispatch }) {
-
     return <button className={operation === '=' && 'span-two'} onClick={() => dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })}>{operation}</button>
 }
